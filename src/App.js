c1ec3fcbbb91c0878a03a56884e20ee7681318a6
Wonahyeon <wah10220@gmail.com>
2023-06-27 17:23:12 +0900

    영화 상세 페이지
    - 가짜 데이터 이용하여 구조만 잡음
    - 라이브러리 설치함(리덕스: api 요청)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import reset from "styled-reset";
 import { createGlobalStyle } from "styled-components";
+import Detail from "./components/Detail";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
