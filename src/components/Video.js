33c11f27972f45f6e5330a37b6b7767adb858ec0
yunjeong0204 <dua0204@naver.com>
2023-06-29 19:00:32 +0900

    비디오 슬라이드 시작 업로드

---
+++ src/components/Video.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const VideoWrapper = styled.div`
+  width: 100%;
+  height: 400px;
+  background-color: white;
+
+
+`;
+
+function Video(props) {
+  return (
+    <>
+      <VideoWrapper>
+
+
+
+      </VideoWrapper>
+    </>
+  );
+}
+
+export default Video;
